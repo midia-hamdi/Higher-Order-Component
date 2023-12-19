73accf5c5ea686670788d280b9e30f9979108ca0
midia-hamdi <midiahamdi367@gmail.com>
2023-12-19 13:12:20 +0330

    HOC

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import TextInput from './TextInput'
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <TextInput />
+        <TextInput />
+        <TextInput />
+      </>
+    )
+  }
+}
